3cf39a9f83054a325888f844578b9772db042498
zeroshintashinta <153372948+zeroshintashinta@users.noreply.github.com>
2025-08-15 18:58:48 +0700

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,8 @@
+// Menampilkan pesan ketika halaman dimuat
+window.onload = function() {
+    alert("Selamat datang di halaman CV saya,guys!");
+}
+// Menampilkan pesan ketika tombol diklik
+function showMessage() {
+    alert("Terima kasih telah mengunjungi halaman saya!");
+}
